f83272d001f354be029e531a6f86ec1dda73f084
Andre Florindo <andreflorindo@hotmail.com>
2020-02-10 17:56:43 +0100

    add atl end effector

---
+++ kr210_atl_ikfast_manipulator_plugin/update_ikfast_plugin.sh
@@ -0,0 +1,1 @@
+rosrun moveit_kinematics create_ikfast_moveit_plugin.py kr210_atl manipulator kr210_atl_ikfast_manipulator_plugin /home/andre/workspaces/vsl_msc_project_ws/src/kr210_cell_support/kr210_atl_ikfast_manipulator_plugin/src/kr210_atl_manipulator_ikfast_solver.cpp
